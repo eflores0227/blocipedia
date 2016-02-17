a5f59e36031b21009564a06cfb660a874c7ffcc8
Estefania Flores <eflores0227@gmail.com>
2016-02-17 11:36:47 -0800

    Fixes rescue statement

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,25 +1,28 @@
 class ChargesController < ApplicationController
   def create
-    customer = Stripe::Customer.create(
-      email: current_user.email,
-      card: params[:stripeToken]
-    )
+    begin
+      customer = Stripe::Customer.create(
+        email: current_user.email,
+        card: params[:stripeToken]
+      )
 
-    charge = Stripe::Charge.create(
-      customer: customer.id,
-      amount: Amount.default,
-      description: "BigMoney Membership - #{current_user.email}",
-      currency: 'usd'
-    )
+      charge = Stripe::Charge.create(
+        customer: customer.id,
+        amount: Amount.default,
+        description: "BigMoney Membership - #{current_user.email}",
+        currency: 'usd'
+      )
 
-    flash[:notice] = "Thanks for all the money, #{current_user.email}! Feel free to pay my again."
-    redirect_to user_path(current_user)
+      flash[:notice] = "Thanks for all the money, #{current_user.email}! Feel free to pay my again."
+      redirect_to user_path(current_user)
+
+    rescue Stripe::CardError => e
+      flash.now[:alert] = e.message
+      redirect_to new_charge_path
+    end
+
   end
 
-  #  rescue Stripe::CardError => e
-  #    flash.now[:alert] = e.message
-  #    redirect_to new_charge_path
-  #  end
 
   def new
     @stripe_btn_data = {
